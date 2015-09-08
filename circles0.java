c970629badbdb3b418e0ca8e7a6a4d20ebae6705
raymg32 <gershomraymundo@gmail.com>
2015-09-08 16:29:47 -0400

    change 1
    changed the size of the screen and the size of the circle making it look like an eclipse

--- circles0.java
+++ circles0.java
@@ -2,10 +2,10 @@
 
 // Setup:  screen size. //
 void setup() {
-  size(200, 200);                // Try changing the width & height.
+  size(600, 600);                // Try changing the width & height.
 }
 
 // Next frame:  circle at (x,y). //
 void draw() {
-  ellipse( 50,50, 30,30 );       // Try changing the (x,y) and size.
+  ellipse( 567,499, 60,20);       // Try changing the (x,y) and size.
 }
