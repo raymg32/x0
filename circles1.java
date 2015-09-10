03bc9d96ac69953bdd72a522d131db7217577e25
raymg32 <gershomraymundo@gmail.com>
2015-09-09 20:05:19 -0400

    change 2

--- circles1.java
+++ circles1.java
@@ -12,13 +12,24 @@ void setup() {
 
 // Next frame:  draw three circles. //
 void draw() {
+  fill(0,0,67);
+  stroke(255);
   ellipse(400, y , 390, 390);
+  fill(0,150,0);
   ellipse(300, 300, 100, 79);
   ellipse(469, 300, 100, 79);
   ellipse(400,500,40,40 );
+  
+  rect(350,400, 100,50);
+  ellipse(200,300,50,50);
+  //color the ear
+  fill(255,0,0);
+  ellipse(600,300,50,50);
  
   // Move them???? //--
   x=  x + 2;  y=  y + 1;
   // Insert BREAK here ^
   // to uncomment the above code,
+  //name charlie
+  
 }
